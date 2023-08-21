e6364693cc698305c2755ab380e6bc6a58edb0ef
Chuck Curtis <cgcurtisiii@gmail.com>
2023-08-21 15:41:14 -0700

    PEP8 edits, add main function idiom

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from youtube_transcript_api import YouTubeTranscriptApi
 api_key = ""  # replace it with your API key
 channel_id = 'UChivF4Th30mhk2e3pGLY6_w'  # Lu's Retro Source
 youtube = build('youtube', 'v3', developerKey=api_key)
- 
-def get_channel_videos(channel_id):
+
+
+def get_channel_videos(channel_id_):
  
     # get Uploads playlist id
-    res = youtube.channels().list(id=channel_id,
+    res = youtube.channels().list(id=channel_id_,
                                   part='contentDetails').execute()
     playlist_id = res['items'][0]['contentDetails']['relatedPlaylists']['uploads']
     videos = []
@@ -28,20 +29,25 @@ def get_channel_videos(channel_id):
             break
  
     return videos
- 
-videos = get_channel_videos(channel_id)
-video_ids = []  # list of all video_id of channel
- 
-for video in videos:
-    video_ids.append(video['snippet']['resourceId']['videoId'])
- 
-for video_id in video_ids:
-    try:
-        responses = YouTubeTranscriptApi.get_transcript(
-            video_id, languages=['en'])
-        print('\n'+"Video: "+"https://www.youtube.com/watch?v="+str(video_id)+'\n'+'\n'+"Captions:")
-        for response in responses:
-            text = response['text']
-            print(text)
-    except Exception as e:
-        print(e)
+
+
+def main():
+    videos = get_channel_videos(channel_id)
+    video_ids = []  # list of all video_id of channel
+
+    for video in videos:
+        video_ids.append(video['snippet']['resourceId']['videoId'])
+
+    for video_id in video_ids:
+        try:
+            responses = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
+            print(f"\nVideo: https://www.youtube.com/watch?v={video_id}\n\nCaptions:")
+            for response in responses:
+                text = response['text']
+                print(text)
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    main()
