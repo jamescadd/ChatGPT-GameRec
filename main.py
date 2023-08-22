cf9f4f00088f0233487157599b4469f03e34c3f1
Chuck Curtis <cgcurtisiii@gmail.com>
2023-08-21 22:45:48 -0700

    extract captions/transcripts to file, use progress bar

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import argparse
 import json
 import os
 
+from alive_progress import alive_bar
 from apiclient.discovery import build
 import langchain
 import openai
@@ -44,26 +45,33 @@ def main(args_):
     # set OPENAI_API_KEY env variable for langchain
     os.putenv("OPENAI_API_KEY", config.get("openai").get("api_key"))
 
-    videos = get_channel_videos(config['youtube'])
-    video_ids = []  # list of all video_id of channel
+    videos = get_channel_videos(config.get('youtube'))
+    video_ids = [video['snippet']['resourceId']['videoId'] for video in videos]  # list of all video_id of channel
 
-    for video in videos:
-        video_ids.append(video['snippet']['resourceId']['videoId'])
+    print(f"Extracting {len(video_ids)} transcripts from channel ID {config.get('youtube').get('channel_id')}")
 
-    for video_id in video_ids:
-        try:
-            responses = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
-            print(f"\nVideo: https://www.youtube.com/watch?v={video_id}\n\nCaptions:")
-            for response in responses:
-                text = response['text']
-                print(text)
-        except Exception as e:
-            print(e)
+    with alive_bar(len(video_ids)) as bar, open(args.output_transcripts, 'w') as o:
+        output_json = []
+
+        for video_id in video_ids:
+            try:
+                responses = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
+                url = f"https://www.youtube.com/watch?v={video_id}"
+                captions = [response['text'] for response in responses]
+                output_json.append({"url": url, "captions": captions})
+            except Exception as e:
+                print(e)
+            finally:
+                bar()
+
+        json.dump(output_json, o, indent=2)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='ChatGPT GameRec')
     parser.add_argument('-c', '--config', metavar='config', type=str,
                         help='JSON config file', default='config.json')
+    parser.add_argument('-ot', '--output-transcripts', metavar='output_transcripts', type=str,
+                        help='JSON output file for video transcripts', default='output_transcripts.json')
     args = parser.parse_args()
     main(args)
