5b01d221b6339bad2d12f9a115c70a193b8c0df6
James Cadd <jamescadd@gmail.com>
2023-10-25 21:38:35 -0700

    Stream echo chatbot sample added to Chat.py

--- Chat.py
+++ Chat.py
@@ -2,4 +2,20 @@ import streamlit as st
 
 st.set_page_config(page_title="Chat",page_icon="👾")
 
-st.write("# 👾 Chat about games")
+st.title("# 🕹️ Game recommendations ")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Ask a question [q or quit to exit]"):
+    st.chat_message("user").markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = f"Echo: {prompt}"
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    st.session_state.messages.append({"role": "assistant", "content": response})
